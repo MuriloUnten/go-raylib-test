37232d89e91a54bd7daa37cdab68ffc695c18d12
MuriloUnten <murilo.unten@proton.me>
2024-12-25 16:02:59 -0300

    basic raylib working

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import(
-    "fmt"
     "github.com/gen2brain/raylib-go/raylib"
 )
 
 func main() {
-    fmt.Println("hello, world")
+	rl.InitWindow(800, 450, "Hello from window")
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.SkyBlue)
+		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.RayWhite)
+
+		rl.EndDrawing()
+	}
 }
